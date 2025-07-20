fa5e6e61d4f0e8a651be4cfe7a38a2bc411b4931
Muhammad Yahya <m.yahya@neo-shift.com>
2025-07-20 11:49:39 +0500

    Fixes

--- app/classified-jobs/page/[page]/page.tsx
+++ app/classified-jobs/page/[page]/page.tsx
@@ -1,14 +1,21 @@
-// app/jobs/page/[page]/page.tsx
+// app/jobs/[page]/page.tsx
 import JobListSection from '@/app/classified-jobs/JobListSection';
 import { notFound } from 'next/navigation';
 
 interface PageProps {
-    params: {
-      page: string;
-    };
-  }
+  params: {
+    page: string;
+  };
+}
 
 export const revalidate = 60;
+
+export async function generateStaticParams() {
+  return Array.from({ length: 5 }).map((_, i) => ({
+    page: (i + 1).toString(),
+  }));
+}
+
 export default async function PaginatedJobsPage({ params }: PageProps) {
   const currentPage = parseInt(params.page, 10);
 
@@ -18,10 +25,9 @@ export default async function PaginatedJobsPage({ params }: PageProps) {
 
   return (
     <div className="max-w-7xl mx-auto px-4 py-10">
-     <section className="mb-6 text-center">
+      <section className="mb-6 text-center">
         <p className="text-sm text-gray-500">9th September, 2001 - 9th September, 2001</p>
       </section>
-      {/* Pass the current page to JobListSection */}
       <JobListSection page={currentPage} />
     </div>
   );
