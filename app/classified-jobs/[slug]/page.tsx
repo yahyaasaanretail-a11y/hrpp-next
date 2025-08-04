7a614f527da41151852ccaceaf56b9b3d0ae2b57
Muhammad Yahya <m.yahya@neo-shift.com>
2025-08-04 20:02:30 +0500

    Fixed

--- app/classified-jobs/[slug]/page.tsx
+++ app/classified-jobs/[slug]/page.tsx
@@ -103,7 +103,7 @@ export default async function Page({
           </a>
           .
         </p>
-        <p className="mt-2">Note: Do not send us your resume.</p>
+        <p className="mt-2">Note: Do not send your resume or contact us by phone.</p>
       </div>
 
       <div className="mt-6 p-4 bg-gray-100 border border-gray-300 rounded-lg shadow-sm text-gray-700 break-words">
